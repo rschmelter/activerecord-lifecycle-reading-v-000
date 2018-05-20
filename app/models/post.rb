6321f7eaaaf54f3e24f9524bf81f064b8a5f45b7
Ryan Schmelter <rschmelter33@gmail.com>
2018-05-20 16:40:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ActiveRecord::Base
 
   private
 
+  def make_title_case
+    self.title = self.title.titlecase
+  end
+
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
       errors.add(:title, "Title must be in title case")
